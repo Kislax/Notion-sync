1465f2d0553902479bbb512361a7c82ef9760837
Maxim Podstrechny <maxpodstrechny@gmail.com>
2022-02-02 23:22:33 +0200

    inititial project

---
+++ Index.js
@@ -0,0 +1,83 @@
+const { Worker } = require('worker_threads');
+require('dotenv').config()
+const { Client, APIErrorCode, LogLevel } = require("@notionhq/client")
+
+const tgWorker = new Worker('./bot.js');
+
+// Initializing a client
+const notion = new Client({
+    auth: process.env.NOTION_TOKEN,
+});
+
+const databaseId = process.env.NOTION_DATABASE_ID
+
+
+const addPage = async (title) => {
+    try {
+        const response = await notion.pages.create({
+            parent: { database_id: databaseId },
+            properties: {
+                title: {
+                    title: [
+                        {
+                            "text": {
+                                "content": title
+                            }
+                        }
+                    ]
+                }
+            },
+        })
+        console.log(response)
+        console.log("Success! Entry added.")
+    } catch (error) {
+        console.error(error.body)
+    }
+}
+
+const showDatabase = async () => {
+    try {
+        const response = await notion.databases.query({
+            database_id: databaseId
+        })
+        // console.log(response.properties)
+        tgWorker.postMessage(response)
+    }
+    catch (error) {
+        console.log(error.body)
+    }
+}
+
+const deleteBlock = async (blockId) => {
+    try {
+        const response = await notion.blocks.delete({
+            block_id: blockId
+        })
+        console.log(response)
+    }
+    catch (error) {
+        console.log(error.body)
+    }
+}
+
+const search = async (searchText) => {
+    const response = await notion.search({
+        query: searchText,
+        sort: {
+            direction: 'ascending',
+            timestamp: 'last_edited_time',
+        },
+    });
+    console.log(response);
+}
+
+// addItem("Сяся маладец")
+// showDatabase()
+// deleteBlock()
+tgWorker.on('message', msg => {
+    console.log('message worker', msg)
+    switch (msg.type) {
+        case 'add': addPage(msg.text);
+        case 'show': showDatabase()
+    }
+})
